d2d4471f2193126cf8c10ff032875e4918d7ee84
Ryan Zemlin <rzemlin@gmail.com>
2021-02-15 22:44:16 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    if yield(collection[i])
+      yield(collection[i]) if
       return yield(collection[i])
     end
     i += 1
